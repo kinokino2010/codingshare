77a89374b7e8bcb98eca2d3a9b89da3a4ef8118c
KINOKINO <kinokino@mail>
2014-01-09 20:21:49 +0900

    test

--- index.js
+++ index.js
@@ -116,6 +116,7 @@ var socketio=require('socket.io');
 var io=socketio.listen(server);
 io.set(‘transports’,['xhr-polling']);
 
+
 var sessionSockets=new SessionSockets(io,store,parseCookie);
 room.listen(io,sessionSockets);
 
